3ffc7eba24ac87d5937f7333fa37891255cfc042
unknown <mateovandamme1@gmail.com>
2026-03-24 20:54:05 +0100

    add working arduino uno side that displays demo data to the ledbart, and working esp32c3 mini versions that send stuff over UART to nothing atm

---
+++ scripts/led_bart_esp32c3_webserver/led_bart_esp32c3_webserver.ino
@@ -0,0 +1,41 @@
+#include <WiFi.h>
+#include <WebServer.h>
+#include <HardwareSerial.h>
+
+#define WIFI_SSID "0x20"
+#define WIFI_PASS "unicorns"
+
+// UART0 default pins: D6=TX, D7=RX
+HardwareSerial UnoSerial(0);
+
+WebServer server(80);
+
+void handleText() {
+  if (!server.hasArg("plain")) {
+    server.send(400, "text/plain", "no body");
+    return;
+  }
+  String body = server.arg("plain");
+  Serial.println("Received: " + body);
+  UnoSerial.println(body);
+  server.send(200, "text/plain", "ok");
+}
+
+void setup() {
+  Serial.begin(115200);
+  UnoSerial.begin(9600, SERIAL_8N1, -1, -1);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("\nIP: " + WiFi.localIP().toString());
+  
+  server.on("/text", HTTP_POST, handleText);
+  server.begin();
+}
+
+void loop() {
+  server.handleClient();
+}
