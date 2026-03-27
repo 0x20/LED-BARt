9827b05fc596cdf05805b13af3ac123ca177148b
unknown <mateovandamme1@gmail.com>
2026-03-27 02:12:42 +0100

    add logging endpoint

--- scripts/led_bart_esp32c3_webserver/led_bart_esp32c3_webserver.ino
+++ scripts/led_bart_esp32c3_webserver/led_bart_esp32c3_webserver.ino
@@ -12,36 +12,61 @@ HardwareSerial UnoSerial(0);
 
 WebServer server(80);
 
+// Log buffer served at GET /log
+#define LOG_MAX 4096
+String logBuffer;
+
+// Outputs to Serial, LED bar, and log buffer
+void out(const String &msg) {
+  Serial.println(msg);
+  UnoSerial.println(msg);
+  logBuffer += msg + "\n";
+  if (logBuffer.length() > LOG_MAX)
+    logBuffer = logBuffer.substring(logBuffer.length() - LOG_MAX);
+}
+
+// Outputs to Serial and log buffer only (no LED bar)
+void log(const String &msg) {
+  Serial.println(msg);
+  logBuffer += msg + "\n";
+  if (logBuffer.length() > LOG_MAX)
+    logBuffer = logBuffer.substring(logBuffer.length() - LOG_MAX);
+}
+
 void handleText() {
   if (!server.hasArg("plain")) {
     server.send(400, "text/plain", "no body");
     return;
   }
   String body = server.arg("plain");
-  Serial.println("Received: " + body);
-  UnoSerial.println(body);
+  log("Received: " + body);
+  out(body);
   server.send(200, "text/plain", "ok");
+}
+
+void handleLog() {
+  server.send(200, "text/plain", logBuffer);
 }
 
 void setup() {
   Serial.begin(115200);
   UnoSerial.begin(9600, SERIAL_8N1, -1, -1);
   WiFi.begin(WIFI_SSID, WIFI_PASS);
-  Serial.print("Connecting to WiFi");
+  log("Connecting to WiFi...");
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
-    Serial.print(".");
   }
-  Serial.println("\nIP: " + WiFi.localIP().toString());
+  log("IP: " + WiFi.localIP().toString());
 
   if (MDNS.begin(MDNS_HOSTNAME)) {
     MDNS.addService("http", "tcp", 80);
-    Serial.println("mDNS: http://" MDNS_HOSTNAME ".local");
+    log("mDNS: http://" MDNS_HOSTNAME ".local");
   } else {
-    Serial.println("mDNS failed");
+    log("mDNS failed");
   }
 
   server.on("/text", HTTP_POST, handleText);
+  server.on("/log", HTTP_GET, handleLog);
   server.begin();
 }
 
